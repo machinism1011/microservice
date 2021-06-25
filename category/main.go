179279720b30863d20f570bef2b33c4ad38ca0df
machinism1101 <737339845lee@gmail.com>
2021-06-25 10:45:29 +0800

    main comments

--- category/main.go
+++ category/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/jinzhu/gorm"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"github.com/machinism1011/microservice/category/common"
 	"github.com/machinism1011/microservice/category/domain/repository"
 	cservice "github.com/machinism1011/microservice/category/domain/service"
